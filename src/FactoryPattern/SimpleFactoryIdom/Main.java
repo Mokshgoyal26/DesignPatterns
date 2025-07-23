bcd99a9cd8637ce0885062d1f5dc27acbd09b745
Moksh <mokshgoyal26@gmail.com>
2025-07-23 13:17:59 +0530

    explained this principle

--- src/FactoryPattern/SimpleFactoryIdom/Main.java
+++ src/FactoryPattern/SimpleFactoryIdom/Main.java
@@ -1,5 +1,9 @@
 package FactoryPattern.SimpleFactoryIdom;
 
+// we are just basically collecting all the object creation of concrete classes and putting into
+// simple factory class which will handle creating objects and make out program clean .
+// remember this is not a factory pattern . still this will violate the ocp principle because for every new type of pizza
+// we have to change the existing code ./
 public class Main {
     public static void main(String[] args){
         SimplePizzaFactory factory = new SimplePizzaFactory();
